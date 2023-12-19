44ca369d2d6aaa730a2c4a683752a528bd293436
Mohammadobe <mobeidatas26@gmail.com>
2023-12-19 11:34:59 +0300

    Initial commit

---
+++ dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,28 @@
+void main() {
+  print('Mohammad');
+
+  String firstStr = 'Mohammad';
+      print(firstStr);
+  int firstInt = 8;
+      print(firstInt);
+  double firstDob = 16.8;
+      print(firstDob);
+  bool firstBoo = true;
+      print(firstBoo);
+
+  var salary = 400;
+      salary = 500;
+      print(salary);
+
+  var salary1 = 400.5;
+      salary1 = 500.5;
+      print(salary1);
+
+  const tax = 0.08;
+      //tax = 0.05;
+      print(tax);
+
+  final newTax = 0.05;
+      //newTax = 0.08;
+      print(newTax);
+}
